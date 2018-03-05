9890e0b2f13dad2f3b2bad813f916712318ef982
muyuanqiang <yuanqiang.mou@funi365.com>
2018-03-05 10:45:19 +0800

    initiailize commit

---
+++ src/main/java/com/funi/muyq/demo/study/App.java
@@ -0,0 +1,13 @@
+package com.funi.muyq.demo.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
